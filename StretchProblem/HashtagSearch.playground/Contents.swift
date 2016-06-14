b403eee554cf1b943b7190e0218e57c7b039f732
Emily Mearns <mearnsem@gmail.com>
2016-06-14 10:14:34 -0600

    Final Commit

--- StretchProblem/HashtagSearch.playground/Contents.swift
+++ StretchProblem/HashtagSearch.playground/Contents.swift
@@ -4,20 +4,13 @@ import UIKit
 
 class Restaurant {
     
-    var name: String
-    var streetAddress: String
-    var city: String
-    var state: String
+    let name: String
+    let streetAddress: String
+    let city: String
+    let state: String
     var reviews: [Review]
     
-    var hashtags: [String] {
-        let hashtag: String = "#"
-        
-        
-        return [hashtag]
-    }
-    
-    init(name: String, streetAddress: String, city: String, state: String, reviews: [Review]) {
+    init(name: String, streetAddress: String, city: String, state: String, reviews: [Review] = []) {
         self.name = name
         self.streetAddress = streetAddress
         self.city = city
@@ -28,8 +21,19 @@ class Restaurant {
 
 class Review {
     
-    var username: String
-    var text: String
+    let username: String
+    let text: String
+    
+    var hashtags: [String] {
+        let words = text.componentsSeparatedByString(" ")
+        var hashtagsArray: [String] = []
+        for word in words {
+            if word.hasPrefix("#") {
+                hashtagsArray.append(word)
+            }
+        }
+        return hashtagsArray
+    }
     
     init(username: String, text: String) {
         self.username = username
@@ -37,7 +41,7 @@ class Review {
     }
 }
 
-let lucky13Review1 = Review(username: "Em", text: "Great burgers! #garlicfries")
+let lucky13Review1 = Review(username: "Em", text: "Great burgers! #garlicfries #stinkycheese")
 let lucky13Review2 = Review(username: "Kent", text: "Get the stinky cheese!")
 let lucky13Reviews = [lucky13Review1, lucky13Review2]
 
@@ -49,4 +53,6 @@ let lucky13 = Restaurant(name: "Lucky 13", streetAddress: "135 1300 S", city: "S
 let cutlers = Restaurant(name: "Cutlers", streetAddress: "500 S", city: "Bountiful", state: "UT", reviews: cutlersReviews)
 
 let restaurants = [lucky13, cutlers]
+
+print(lucky13Review1.hashtags)
 
